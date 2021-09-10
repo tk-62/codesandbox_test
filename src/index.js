50d96e7e01697c927455756b0a223b09bf0ee27e
tk <68478701+tk-62@users.noreply.github.com>
2021-09-11 01:15:49 +0900

    テンプレート文字列追加

--- src/index.js
+++ src/index.js
@@ -47,3 +47,18 @@
 // val5[0] = "bird";
 // val5.push("monkey");
 // console.log(val5);
+
+/**
+ * テンプレート文字列
+ */
+const name = "じゃけぇ";
+const age = 28;
+//「私の名前はじゃけぇです。年齢は２８歳です。」
+
+//従来の方法
+const message1 = "私の名前は" + name + "です。年齢は" + age + "歳です。";
+console.log(message1);
+
+//テンプレート文字列を用いた方法
+const message2 = `私の名前は${name}です。年齢は${age}歳です。`;
+console.log(message2);
