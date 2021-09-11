c97df4fe7948f78b0cc955af3cf11a5d39d8496b
tk <68478701+tk-62@users.noreply.github.com>
2021-09-12 06:38:19 +0900

    三項演算子途中

--- src/index.js
+++ src/index.js
@@ -162,9 +162,9 @@
  * mapやfilterを使った配列の処理
  */
 
-const nameArr = ["田中", "山田", "じゃけぇ"];
+// const nameArr = ["田中", "山田", "じゃけぇ"];
 //  for( let index = 0; index < nameArr.length; index++){
-//    console.log(nameArr[index]);
+//    console.log(`${index + 1}番目は${nameArr[index]}です。`);
 //  }
 
 //  const nameArr2 = nameArr.map((name) => {
@@ -172,4 +172,37 @@ const nameArr = ["田中", "山田", "じゃけぇ"];
 //  });
 // console.log(nameArr2);
 
-nameArr.map((name) => console.log(name));
+// nameArr.map((name, index) => console.log(`${index + 1}番目は${name}です`));
+
+// const numArr = [1,2,3,4,5];
+// const newNumArr = numArr.filter((num) => {
+//   return num % 2 === 0;
+// });
+// console.log(newNumArr);
+
+// const newNameArr = nameArr.map((name) => {
+//   if(name === 'じゃけぇ'){
+//     return(name)
+//   } else {
+//     return `${name}さん`
+//   }
+// });
+// console.log(newNameArr);
+
+/**
+ * 三項演算子
+ */
+//ある条件 ? 条件がtrueの時 : 条件がfalseの時
+// const val1 = 0 > 1 ? `trueです` : 'falseです';
+// console.log(val1);
+
+// const num = "1300";
+// console.log(num.toLocaleString());
+
+// const formattedNum = typeof num === 'number' ? num.toLocaleString() : '数字を入力して下さい';
+// console.log(formattedNum);
+
+const checkSum = (num1, num2) => {
+  return num1 + num2 > 100 ? "100を超えています!!" : "許容範囲内です";
+};
+console.log(checkSum(50, 60));
